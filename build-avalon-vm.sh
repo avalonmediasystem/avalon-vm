6ee3284f6eab644e539e7fac9140c9b8fd8010ff
Chris Colvard <cjcolvar@indiana.edu>
2014-04-11 12:06:27 -0400

    Bump version

--- build-avalon-vm.sh
+++ build-avalon-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fpm --force -s dir -t rpm -n avalon-vm -v 1.0 --iteration 1 --license GPL --url http://avalonmediasystem.org --description "Virtual machine distribution setup for Avalon Media System" --rpm-os linux -a noarch --after-install postinst --prefix /usr/share avalon-vm
+fpm --force -s dir -t rpm -n avalon-vm -v 1.1 --iteration 1 --license GPL --url http://avalonmediasystem.org --description "Virtual machine distribution setup for Avalon Media System" --rpm-os linux -a noarch --after-install postinst --prefix /usr/share avalon-vm
