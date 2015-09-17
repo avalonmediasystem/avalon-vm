5b26c668b87094f8d476d66ac261fe608ecda8f3
Michael Klein <mbklein@gmail.com>
2015-09-17 00:07:53 -0500

    Update for Avalon 4.0

--- build-avalon-vm.sh
+++ build-avalon-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fpm --force -s dir -t rpm -n avalon-vm -v 2.1 --iteration 1 --license GPL --url http://avalonmediasystem.org --description "Virtual machine distribution setup for Avalon Media System" --rpm-os linux -a noarch --after-install postinst --prefix /usr/share avalon-vm
+fpm --verbose --force -s dir -t rpm -n avalon-vm -v 2.2 --iteration 1 --license GPL --url http://avalonmediasystem.org --description "Virtual machine distribution setup for Avalon Media System" --rpm-os linux -a noarch --after-install postinst --prefix /usr/share avalon-vm
